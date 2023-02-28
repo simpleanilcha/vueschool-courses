dcaf0f878f43c5e35cc7b308d6b0d88c5a133561
Anil Maharjan <simpleanilcha@gmail.com>
2023-02-28 22:47:30 +0545

    user events in vue 3

--- fundamentals-composition-api/src/App.vue
+++ fundamentals-composition-api/src/App.vue
@@ -11,37 +11,19 @@ const items = ref([
 
 const newItem = ref('')
 const newItemHighPriority = ref('')
-const newItemPriority = ref('low')
-const iceCreamFlavors = ref([])
-const newItemColor = ref('')
+
 </script>
 
 <template>
   <h1>{{ header }}</h1>
-  <input type="text" v-model.trim="newItem" placeholder="Add an item">
-  <!-- <input type="checkbox" v-model="newItemHighPriority" name="" id=""> High Priority -->
 
-  <input type="radio" v-model="newItemHighPriority" value="low"> Low
-  <input type="radio" v-model="newItemHighPriority" value="high"> High
-  <!-- {{ newItemHighPriority }} -->
-
-  <input type="color" v-model="newItemColor"> Choose color
-  {{ newItemColor }}
-
-  <select v-model="newItemPriority" name="" id="">
-    <option value="low">Low</option>
-    <option value="medium">Medium</option>
-    <option value="high">High</option>
-  </select>
-  <!-- {{ newItemPriority }} -->
-
-  <label for="chocolate">Chocolate</label>
-  <input type="checkbox" v-model="iceCreamFlavors" id="chocolate" value="chocolate">
-  <label for="vanilla">vanilla</label>
-  <input type="checkbox" v-model="iceCreamFlavors" id="vanilla" value="vanilla">
-  <label for="strawberry">strawberry</label>
-  <input type="checkbox" v-model="iceCreamFlavors" id="strawberry" value="strawberry">
-
+  <form class="add-item-form" @submit.prevent="items.push({ id: items.length + 1, label: newItem })">
+    <input type="text" v-model.trim="newItem" placeholder="Add an item">
+    <input type="checkbox" v-model="newItemHighPriority" name="" id=""> High Priority
+    <button class="btn btn-primary">
+      Save Item
+    </button>
+  </form>
 
   <ul>
     <li v-for="({ id, label }, index) in items" :key="id">
