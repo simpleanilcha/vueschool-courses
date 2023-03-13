5888bea48e4f90893114980bb7306d3afd87d719
Anil Maharjan <simpleanilcha@gmail.com>
2023-03-13 13:45:42 +0545

    communicatio betn components with custom events - emit

--- vue3-components-fundamentals/app.js
+++ vue3-components-fundamentals/app.js
@@ -5,6 +5,16 @@ const PlanComponent = {
       type: String,
       default: 'Hello',
       required: true
+    },
+    selected: {
+      type: Boolean,
+      default: false,
+      required: true
+    }
+  },
+  methods: {
+    select() {
+      this.$emit('select', this.name)
     }
   }
 }
@@ -13,10 +23,16 @@ const PlanPickerComponent = {
   components: { Plan: PlanComponent },
   data() {
     return {
-      plans: ['The Single', 'The Curious', 'The Addict']
+      plans: ['The Single', 'The Curious', 'The Addict'],
+      selectedPlan: null,
     }
   },
   template: '#plan-picker-template',
+  methods: {
+    selectPlan(plan) {
+      this.selectedPlan = plan
+    }
+  }
 }
 
 const app = Vue.createApp({
