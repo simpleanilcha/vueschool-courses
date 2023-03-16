08e1ad8e0ab3d90bbf2a9d01df9fbeaa63fc322a
Anil Maharjan <simpleanilcha@gmail.com>
2023-03-16 18:42:47 +0545

    adding routes and content

--- vue3-router4/vue-school-travel-app/src/router/index.js
+++ vue3-router4/vue-school-travel-app/src/router/index.js
@@ -1,5 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import BrazilView from '../views/BrazilView.vue'
+import HawaiiView from '../views/HawaiiView.vue'
+import JamaicaView from '../views/JamaicaView.vue'
+import PanamaView from '../views/PanamaView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,7 +20,27 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
-    }
+    },
+    {
+      path: '/brazil',
+      name: 'brazil',
+      component: BrazilView
+    },
+    {
+      path: '/hawaii',
+      name: 'hawaii',
+      component: HawaiiView
+    },
+    {
+      path: '/jamaica',
+      name: 'jamaica',
+      component: JamaicaView
+    },
+    {
+      path: '/panama',
+      name: 'panama',
+      component: PanamaView
+    },
   ]
 })
 
--- vue3-router4/vue-school-travel-app/src/views/HomeView.vue
+++ vue3-router4/vue-school-travel-app/src/views/HomeView.vue
@@ -1,9 +1,20 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import sourceData from '@/data.json'
+
+const destinations = sourceData.destinations
+console.log(destinations)
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home">
+    <h1>All Destinations</h1>
+
+    <div class="destinations">
+      <RouterLink v-for="destination in destinations" :key="destination.id" :to="destination.slug">
+        <h2>{{ destination.name }}</h2>
+        <img :src="`/images/${destination.image}`" :alt="destination.name">
+      </RouterLink>
+    </div>
+
+  </div>
 </template>
